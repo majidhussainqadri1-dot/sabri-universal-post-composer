f363c355fa0606485f9e763cab951b70204c34b0
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-07-29 05:44:59 +0500

    ci: add plugin lifecycle signatures to PHPStan stubs

--- tests/phpstan-wordpress-stubs.php
+++ tests/phpstan-wordpress-stubs.php
@@ -9,8 +9,36 @@ if ( ! defined( 'SUPC_WORKFLOW_API_VERSION' ) ) {
 	define( 'SUPC_WORKFLOW_API_VERSION', '1.0.0' );
 }
 
+if ( ! defined( 'SUPC_FILE' ) ) {
+	define( 'SUPC_FILE', __FILE__ );
+}
+
 if ( ! function_exists( 'wp_json_encode' ) ) {
 	function wp_json_encode( mixed $value, int $flags = 0, int $depth = 512 ): string|false {
 		return json_encode( $value, $flags, $depth );
+	}
+}
+
+if ( ! function_exists( 'load_plugin_textdomain' ) ) {
+	function load_plugin_textdomain( string $domain, bool $deprecated = false, string $plugin_rel_path = '' ): bool {
+		unset( $domain, $deprecated, $plugin_rel_path );
+		return true;
+	}
+}
+
+if ( ! function_exists( 'plugin_basename' ) ) {
+	function plugin_basename( string $file ): string {
+		return basename( $file );
+	}
+}
+
+if ( ! function_exists( 'add_shortcode' ) ) {
+	function add_shortcode( string $tag, callable $callback ): void {
+		unset( $tag, $callback );
+	}
+}
+
+if ( ! function_exists( 'nocache_headers' ) ) {
+	function nocache_headers(): void {
 	}
 }
